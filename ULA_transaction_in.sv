ea5b8d69573cb85ba2a81e01a784041ece8c987f
João Pedro Melquiades Gomes <joao.gomes@embedded.ufcg.edu.br>
2019-10-23 11:15:27 -0300

    Implementaçao do driver, monitor e inserção de todos no agent

--- ULA_transaction_in.sv
+++ ULA_transaction_in.sv
@@ -1,9 +1,8 @@
 class ULA_transaction_in extends uvm_sequence_item;​
 
   rand bit [16:0]  data_in;
-  rand_bit [1:0]   reg_sel;
-  rand_bit [1:0]   instru;
-  rand bit         valid_ula;
+  rand bit [1:0]   reg_sel;
+  rand bit [1:0]   instru;
 
 
   function new(string name = "");​
@@ -14,11 +13,11 @@ class ULA_transaction_in extends uvm_sequence_item;​
     `uvm_field_int(data_in , UVM_UNSIGNED)​
     `uvm_field_int(reg_sel , UVM_UNSIGNED)​
     `uvm_field_int(instru , UVM_UNSIGNED)​
-    `uvm_field_int(valid_ula , UVM_UNSIGNED)​
+    //`uvm_field_int(valid_ula , UVM_UNSIGNED)​
   `uvm_object_utils_end​
 
   function string convert2string();​
-    return $sformatf("{data_in = %d, reg_sel = %d, instru = %d, valid_ula = %d}",data_in, reg_sel, instru, valid_ula);​
+    return $sformatf("{data_in = %d, reg_sel = %d, instru = %d}",data_in, reg_sel, instru);​
 
   endfunction​
 
