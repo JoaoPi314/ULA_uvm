5965b12ae77767e98fddc8bb1b74dad794ed1b94
Débora Nunes Pinto de Oliveira <debora.oliveira@embedded.ufcg.edu.br>
2019-11-13 07:41:17 -0300

    Cobertura 100%. FINALMENTE. Ajuste no monitor e no driver da ULA

--- ambiente/tb/xcelium.d/run.lnx8664.18.09.d/files.ts
+++ ambiente/tb/xcelium.d/run.lnx8664.18.09.d/files.ts
@@ -1,8 +1,8 @@
-1573477341 /mnt/hdd1tera/xmen/debora/ULA_uvm/ambiente/tb/pkg.sv
-1573477341 /mnt/hdd1tera/xmen/debora/ULA_uvm/ambiente/tb/ULA_interface_if.sv
-1573477341 /mnt/hdd1tera/xmen/debora/ULA_uvm/ambiente/tb/REG_interface_if.sv
-1573483193 /mnt/hdd1tera/xmen/debora/ULA_uvm/ambiente/rtl/ula.sv
-1573477341 /mnt/hdd1tera/xmen/debora/ULA_uvm/ambiente/rtl/rb.sv
-1573477341 /mnt/hdd1tera/xmen/debora/ULA_uvm/ambiente/rtl/mux.sv
-1573477341 /mnt/hdd1tera/xmen/debora/ULA_uvm/ambiente/rtl/datapath.sv
-1573477341 /mnt/hdd1tera/xmen/debora/ULA_uvm/ambiente/tb/top.sv
+1573639909 /home/xmen/debora/verificacao/ULA_uvm/ambiente/tb/pkg.sv
+1573639909 /home/xmen/debora/verificacao/ULA_uvm/ambiente/tb/ULA_interface_if.sv
+1573639909 /home/xmen/debora/verificacao/ULA_uvm/ambiente/tb/REG_interface_if.sv
+1573639909 /home/xmen/debora/verificacao/ULA_uvm/ambiente/rtl/ula.sv
+1573639909 /home/xmen/debora/verificacao/ULA_uvm/ambiente/rtl/rb.sv
+1573639909 /home/xmen/debora/verificacao/ULA_uvm/ambiente/rtl/mux.sv
+1573639909 /home/xmen/debora/verificacao/ULA_uvm/ambiente/rtl/datapath.sv
+1573639909 /home/xmen/debora/verificacao/ULA_uvm/ambiente/tb/top.sv
