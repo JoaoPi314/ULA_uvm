46a3ca9793d6bf159210c5f8921084dc8887b21c
joao.gomes <joao.melquiades@ee.ufcg.edu.br>
2020-02-05 14:59:14 -0200

    Removidos arquivos de simulação

--- ambiente/tb/xcelium.d/run.lnx8664.19.09.d/.timestamp.ts
+++
@@ -1,8 +0,0 @@
-1574264615 /mnt/hdd1tera/xmen/Documentos/ULA_uvm/ambiente/rtl/rb.sv
-1574264615 /mnt/hdd1tera/xmen/Documentos/ULA_uvm/ambiente/tb/top.sv
-1574264615 /mnt/hdd1tera/xmen/Documentos/ULA_uvm/ambiente/rtl/datapath.sv
-1574264615 /mnt/hdd1tera/xmen/Documentos/ULA_uvm/ambiente/tb/pkg.sv
-1574264615 /mnt/hdd1tera/xmen/Documentos/ULA_uvm/ambiente/tb/ULA_interface_if.sv
-1574264615 /mnt/hdd1tera/xmen/Documentos/ULA_uvm/ambiente/rtl/mux.sv
-1574264615 /mnt/hdd1tera/xmen/Documentos/ULA_uvm/ambiente/tb/REG_interface_if.sv
-1574264615 /mnt/hdd1tera/xmen/Documentos/ULA_uvm/ambiente/rtl/ula.sv
